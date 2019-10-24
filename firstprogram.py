ef94b7b298430e2fa3f14c7b200d4d93f7fab453
trymeout123 <56973274+trymeout123@users.noreply.github.com>
2019-10-24 22:33:16 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,22 @@
+a=5
+b=9
+a=5.5
+c=a**b
+print(a-2)
+print(c)
+print(type(c))
+print('i"am happiest person')
+print(len("sagar"))
+name="sagarchikhale"
+print(name[5])
+print(name[5:])
+print(name[:5])
+print(name[3:5])
+print(name[::])
+print(name[::2])
+print(name[::-1])
+print(len(name))
+print(type(name))
+print("enter your name")
+n=input()
+print(type(n))
